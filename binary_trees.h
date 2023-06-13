c850b2e0a6441fd96d9c5ffdaa855d27bdca113b
username <email@gmail.com>
2023-06-12 18:19:08 -0700

    Binary Tree Implementations

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,10 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -21,5 +25,10 @@ typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
+
+void binary_tree_print(const binary_tree_t *); /* print binary tree */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
 
 #endif /* BINARY_TREES_H */
