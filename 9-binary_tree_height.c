afaba0dfdb22b5b4e1674544b1473209c3ec4d80
username <email@gmail.com>
2023-06-12 20:10:32 -0700

    Binary Tree Implementations

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,13 +9,18 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	if (tree)
+	{
+		int left_height = 0, right_height = 0;
 
-	if (tree == NULL)
-		return (0);
+		if (tree->left)
+			left_height = 1 + binary_tree_height(tree->left);
+		if (tree->right)
+			right_height = 1 + binary_tree_height(tree->right);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+		return (left_height > right_height ?
+				left_height : right_height);
+	}
 
-	return ((left_height > right_height ? left_height : right_height) + 1);
+	return (0);
 }
