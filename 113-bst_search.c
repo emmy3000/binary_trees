ef7a2acef3c8e4d8f6c848d8233b471f46198a42
username <email@gmail.com>
2023-06-14 06:34:08 -0700

    Binary Tree Implementations

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -18,7 +18,7 @@ bst_t *bst_search(const bst_t *tree, int value)
 		return ((bst_t *)tree);
 
 	if (value < tree->n)
-		return bst_search(tree->left, value);
+		return (bst_search(tree->left, value));
 	else
 		return (bst_search(tree->right, value));
 }
